24181c5b4ea8b594c04bca241c807a663e9c28d8
Morozov312 <alexander.morozov312@gmail.com>
2021-03-30 21:55:23 +0700

    developed styles for progress bar

--- src/Components/TestContainer/ProgressBar/ProgressBar.js
+++ src/Components/TestContainer/ProgressBar/ProgressBar.js
@@ -6,15 +6,19 @@ import './ProgressBar.scss'
 function ProgressBar({barLength, selected}) {
     const progressItems = []
 
-    for (let i = 0; i < barLength; i++) {
-        progressItems.push({id: getId()})
+    for (let i = 0; i < barLength; ++i) {
+        const isActive = (i < selected) ? 'active' : ''
+        progressItems.push({
+            id: getId(),
+            isActive
+        })
     }
 
     return (
         <div className="progressBar">
             {
                 progressItems.map(i => <div key={i.id} className="progressBar__item">
-                    <div className="circle"/>
+                    <div className={`circle ${i.isActive}`}/>
                 </div>)
             }
         </div>
