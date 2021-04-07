4c7a51fa3a2dd760c09013e6a47d1505e7dce05e
Morozov312 <alexander.morozov312@gmail.com>
2021-04-07 23:56:08 +0700

    start styled result section

--- src/Components/Result/Result.js
+++ src/Components/Result/Result.js
@@ -1,9 +1,17 @@
 import React, {useEffect} from 'react'
+import PropTypes from 'prop-types'
+import './Result.scss'
 import data from './resultAnswers.json'
 
 function Result({showWelcome, resultsArr}) {
 
-    const a = resultsArr.reduce((prev, curr) => prev[curr], data)
+    const giftsArr = []
+
+    resultsArr.forEach((i, index) => {
+        const giftsCount = data[index][i].length
+        const randomGiftIndex = Math.floor(Math.random() * giftsCount)
+        giftsArr.push(data[index][i][randomGiftIndex])
+    })
 
     useEffect(() => {
         return () => {
@@ -11,20 +19,27 @@ function Result({showWelcome, resultsArr}) {
         }
     }, [resultsArr]);
 
+
     return (
-        <div>
-            <h2 className="potom">Lorem ipsum dolor sit amet</h2>
-            <pre>
-                Some text Some text Some text Some text <br/>
-                Some text Some text Some text Some text <br/>
-                Some text Some text Some text Some text <br/>
-            </pre>
-            <h3>{a.title}</h3>
-            <pre>{a.description}</pre>
-            <img src={`${process.env.PUBLIC_URL}/images/${a.name}`} alt={a.title}/>
-            <button onClick={() => showWelcome()}>GO HOME</button>
+        <div className="result">
+            {giftsArr.map(i =>
+                <div key={i.filename}>
+                    <h3 className="result__title">{i.title}</h3>
+                    <img
+                        src={`${process.env.PUBLIC_URL}/images/${i.filename}`}
+                        alt={i.title}
+                        className="result__img"
+                    />
+                </div>
+            )}
+            <button className="result__btn" onClick={() => showWelcome()}>Пройти тест снова</button>
         </div>
     )
+}
+
+Result.propTypes = {
+    showWelcome: PropTypes.func,
+    resultsArr: PropTypes.arrayOf(PropTypes.string)
 }
 
 export default Result
